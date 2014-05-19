0665d5cfc0fa7ca3e8b55b5ee02f3d357522cfdf
decagondev <decagondevelopment@gmail.com>
2014-05-19 21:09:38 +0100

    updated main.c

--- src/main.c
+++ src/main.c
@@ -9,12 +9,12 @@
 		switch (*code)
 		{
 			case 0x00: printf("NOP"); break;
-			case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opCodeSize=3; break;
+			case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opCodeSize = 3; break;
 			case 0x02: printf("STAX   B"); break;
 			case 0x03: printf("INX    B"); break;
 			case 0x04: printf("INR    B"); break;
 			case 0x05: printf("DCR    B"); break;
-			case 0x06: printf("MVI    B,#$%02x", code[1]); opCodeSize=2; break;
+			case 0x06: printf("MVI    B,#$%02x", code[1]); opCodeSize = 2; break;
 			case 0x07: printf("RLC"); break;
 			case 0x08: printf("NOP"); break;
 			case 0x09: printf("DAD    B"); break;
