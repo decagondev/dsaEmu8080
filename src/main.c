a798855977ac61f66a5e184457a2eb1323204c8e
decagondev <decagondevelopment@gmail.com>
2014-05-19 21:08:39 +0100

    Minor Updates to main.c
    updated the main.c with a switch case statement and a place holder function on YouTube. the next update will be finishing off the function and adding the rest of the op codes in to the function.

--- src/main.c
+++ src/main.c
@@ -2,34 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int DissAsm() {
-  int insSize = 1; // size of instruction in bytes ( 1 / 2 / 3 )..
-switch (*code)
-{
-	case 0x00: printf("NOP"); return;
-	case 0x01: printf("NOP"); return;
-	case 0x02: printf("NOP"); return;
-	case 0x03: printf("NOP"); return;
-	case 0x04: printf("NOP"); return;
-	case 0x05: printf("NOP"); return;
-	case 0x06: printf("NOP"); return;
-	case 0x07: printf("NOP"); return;
-	case 0x08: printf("NOP"); return;
-	case 0x09: printf("NOP"); return;
-	case 0x0A: printf("NOP"); return;
-	case 0x0B: printf("NOP"); return;
-	case 0x0C: printf("NOP"); return;
-	case 0x0D: printf("NOP"); return;
-	case 0x0E: printf("NOP"); return;
-	case 0x0F: printf("NOP"); return;
-	case 0x10: printf("NOP"); return;
-	case 0x11: printf("NOP"); return;
-	case 0x12: printf("NOP"); return;
-	case 0x13: printf("NOP"); return;
-	case 0x14: printf("NOP"); return;
-	case 0x15: printf("NOP"); return;
-	case 0x16: printf("NOP"); return;
-	case 0x17: printf("NOP"); return;
-}
-
-return insSize;
+	void myFunction() 
+	{
+		char *code;
+		int opCodeSize = 1;
+		switch (*code)
+		{
+			case 0x00: printf("NOP"); break;
+			case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opCodeSize=3; break;
+			case 0x02: printf("STAX   B"); break;
+			case 0x03: printf("INX    B"); break;
+			case 0x04: printf("INR    B"); break;
+			case 0x05: printf("DCR    B"); break;
+			case 0x06: printf("MVI    B,#$%02x", code[1]); opCodeSize=2; break;
+			case 0x07: printf("RLC"); break;
+			case 0x08: printf("NOP"); break;
+			case 0x09: printf("DAD    B"); break;
+			case 0x0a: printf("LDAX   B"); break;
+			case 0x0b: printf("DCX    B"); break;
+			case 0x0c: printf("INR    C"); break;
+			case 0x0d: printf("DCR    C"); break;
+			case 0x0e: printf("MVI    C,#$%02x", code[1]); opCodeSize = 2;	break;
+			case 0x0f: printf("RRC"); break;
+			
+			case 0x10: printf(); break;
+		}
+		
+		
+		
+	}
+		
