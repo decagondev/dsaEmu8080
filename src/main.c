08b942a3c29688a4adc6d5acc6494481a3a06a0e
decagondev <decagondevelopment@gmail.com>
2014-05-19 20:34:33 +0100

    Updated main.c
    Added case statement to main.c and started DissAsm() function
    TODO: finnish off the DissAssm() function.

--- src/main.c
+++ src/main.c
@@ -1,1 +1,35 @@
 /* main.c */
+#include <stdio.h>
+#include <stdlib.h>
+
+int DissAsm() {
+  int insSize = 1; // size of instruction in bytes ( 1 / 2 / 3 )..
+switch (*code)
+{
+	case 0x00: printf("NOP"); return;
+	case 0x01: printf("NOP"); return;
+	case 0x02: printf("NOP"); return;
+	case 0x03: printf("NOP"); return;
+	case 0x04: printf("NOP"); return;
+	case 0x05: printf("NOP"); return;
+	case 0x06: printf("NOP"); return;
+	case 0x07: printf("NOP"); return;
+	case 0x08: printf("NOP"); return;
+	case 0x09: printf("NOP"); return;
+	case 0x0A: printf("NOP"); return;
+	case 0x0B: printf("NOP"); return;
+	case 0x0C: printf("NOP"); return;
+	case 0x0D: printf("NOP"); return;
+	case 0x0E: printf("NOP"); return;
+	case 0x0F: printf("NOP"); return;
+	case 0x10: printf("NOP"); return;
+	case 0x11: printf("NOP"); return;
+	case 0x12: printf("NOP"); return;
+	case 0x13: printf("NOP"); return;
+	case 0x14: printf("NOP"); return;
+	case 0x15: printf("NOP"); return;
+	case 0x16: printf("NOP"); return;
+	case 0x17: printf("NOP"); return;
+}
+
+return insSize;
