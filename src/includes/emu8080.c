ae646d4708f57ef06220469f96a3feae1a832a15
Decagon <decagondevelopment@gmail.com>
2014-06-18 12:50:56 +0100

    Create emu8080.c
    implementation of the functions in emu8080.h

---
+++ src/includes/emu8080.c
@@ -0,0 +1,74 @@
+#include "emu8080.h"
+
+unsigned char cycles8080[] = {
+	4, 10, 7, 5, 5, 5, 7, 4, 4, 10, 7, 5, 5, 5, 7, 4, //0x00..0x0f
+	4, 10, 7, 5, 5, 5, 7, 4, 4, 10, 7, 5, 5, 5, 7, 4, //0x10..0x1f
+	4, 10, 16, 5, 5, 5, 7, 4, 4, 10, 16, 5, 5, 5, 7, 4, //etc
+	4, 10, 13, 5, 10, 10, 10, 4, 4, 10, 13, 5, 5, 5, 7, 4,
+	
+	5, 5, 5, 5, 5, 5, 7, 5, 5, 5, 5, 5, 5, 5, 7, 5, //0x40..0x4f
+	5, 5, 5, 5, 5, 5, 7, 5, 5, 5, 5, 5, 5, 5, 7, 5,
+	5, 5, 5, 5, 5, 5, 7, 5, 5, 5, 5, 5, 5, 5, 7, 5,
+	7, 7, 7, 7, 7, 7, 7, 7, 5, 5, 5, 5, 5, 5, 7, 5,
+	
+	4, 4, 4, 4, 4, 4, 7, 4, 4, 4, 4, 4, 4, 4, 7, 4, //0x80..8x4f
+	4, 4, 4, 4, 4, 4, 7, 4, 4, 4, 4, 4, 4, 4, 7, 4,
+	4, 4, 4, 4, 4, 4, 7, 4, 4, 4, 4, 4, 4, 4, 7, 4,
+	4, 4, 4, 4, 4, 4, 7, 4, 4, 4, 4, 4, 4, 4, 7, 4,
+	
+	11, 10, 10, 10, 17, 11, 7, 11, 11, 10, 10, 10, 10, 17, 7, 11, //0xc0..0xcf
+	11, 10, 10, 10, 17, 11, 7, 11, 11, 10, 10, 10, 10, 17, 7, 11, 
+	11, 10, 10, 18, 17, 11, 7, 11, 11, 5, 10, 5, 17, 17, 7, 11, 
+	11, 10, 10, 4, 17, 11, 7, 11, 11, 5, 10, 4, 17, 17, 7, 11, 
+};
+//parity function
+static int parity(int x, int size)
+{
+	int i;
+	int p = 0;
+	x = (x & ((1<<size)-1));
+	for (i=0; i<size; i++)
+	{
+		if (x & 0x1) p++;
+		x = x >> 1;
+	}
+	return (0 == (p & 0x1));
+}
+
+static void WriteMem(CpuState *state, uint16_t addr, uint8_t val)
+{
+	state->memory[addr] = val;
+}
+
+static uint8_t ReadFromHL(CpuState *state)
+{
+	uint16_t offset = (state->h << 8) | state->l;
+	return state->memory[offset];
+}
+
+static uint8_t WriteToHL(CpuState *state, uint8_t val)
+{
+	uint16_t offset = (state->h << 8) | state->l;
+	WriteMem(state, offset, val);
+}
+
+
+
+static void Push(CpuState *state, uint8_t high, uint8_t low)
+{
+	WriteMem(state, state->sp-1, high);
+	WriteMem(state, state->sp-2, low);
+	state->sp = state->sp -2;
+
+}
+
+void GenerateInterrupt(CpuState* state, int interruptNum)
+{
+	// perform "PUSH PC"
+	Push(state, (state->pc & 0xff00) >> 8, (state->pc & 0xff));
+	//Set PC to low memory Vector.
+	state->pc = 8 * interruptNum;
+
+	// "DI"
+	state->int_enable = 0;
+}
