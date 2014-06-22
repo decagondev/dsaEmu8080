c0b1be7a290d8cda176bf53e4c2289fb1d054203
Decagon <decagondevelopment@gmail.com>
2014-06-22 22:39:31 +0100

    Create DsaMachine.m
    Created the base m file for the DsaMachine.

---
+++ src/DsaMachine.m
@@ -0,0 +1,30 @@
+// DsaMachine.m : part of the DsaEmu8080 Project
+// https://github.com/decagondev/dsaEmu8080
+
+#define USE_THREADS 1
+
+#import "DsaMachine.h"
+#include <sys/time.h>
+
+@implementation DsaMachine
+
+-(void) ReadFile:(NSString*)fileName IntoMemAt:(uint32_t)memOffset
+{
+	NSBundle *mBundle = [MSBundle mainBundle];
+	NSString *filePath = [mBundle pathForResource:fileName ofType:NULL];
+
+	NSData *data = [[NSData alloc]initWithContentsOfFile:filePath];
+
+	if (data == NULL)
+	{
+		NSLog(@"Opening %@ failed.", fileName);
+		return;
+	}
+	if ([data length] > 0x800)
+	{
+		NSLog(@"The file %@ appears to be corrupted! too big at: %ld bytes", fileName, [data length]);
+	}
+	uint8_t *buffer = &state->memory[memOffset];
+	memcpy(buffer, [data bytes], [data length]);
+}
+}
